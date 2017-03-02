c0d5e502efb3042f1a13730a2ba4a8107bbfec76
leyyaps <andrew.smith@ipsofactouk.com>
2017-03-02 13:13:45 +0000

    changed production server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,10 +7,6 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-set :stage, :production
-
-# Replace 127.0.0.1 with your server's IP address!
-server '160.153.224.52', user: 'andrew', roles: %w{app db web}
 
 
 # role-based syntax
